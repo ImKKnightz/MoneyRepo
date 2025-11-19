2d598085284ede6a2320eca593cc99b2d8571029
kaikoon <s10258401@connect.np.edu.sg>
2025-11-19 15:25:29 +0800

    Adding managers into scene
    - Foundations of the game
    - Establishing idea
    - Testing code

---
+++ MoneyGame/Assets/Scripts/SavingGoal.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavingGoal : MonoBehaviour
+{
+    public int goalAmount = 100;
+    public int daysToWin = 14;
+    public int daysLeft = 0;
+
+    public void Goalreached()
+    {
+
+    }
+}
